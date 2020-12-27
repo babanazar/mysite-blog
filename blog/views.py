32a3a1a6cbd17e4dc7919c097a3c3b63476bcb7c
Babanazar Gutlygeldiyev <babanazar@threadinmotion.com>
2020-12-27 16:28:38 +0300

    pagination detail and list functionalities added

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 
 def post_detail(request, year, month, day, post):
@@ -17,5 +20,4 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     return render(request,
                   'blog/post/detail.html',
-                  {'post', post})
-
+                  {'post': post})
