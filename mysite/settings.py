a841e421cf74c23c630972ce95e99a1ea97a54ca
Babanazar Gutlygeldiyev <babanazar@threadinmotion.com>
2020-12-27 18:53:18 +0300

    sharing a post is complete

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'barlapgor@gmail.com'
+EMAIL_HOST_PASSWORD = 'thhcrovrcgcxcdgy'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
